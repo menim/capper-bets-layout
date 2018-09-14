d485168f8109193b300f4f063539a7da76bd21e3
tol <tol@Mac-mini-tol.local>
2018-09-14 17:56:46 +0300

    add responsive to logo module

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/*var elem = document.querySelector('.capper-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  groupCells: true
+  pageDots: false,
+  prevNextButtons: false
+});
+*/
